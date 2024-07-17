b47d5a57b247906fd671c49cf96cd4d364a030f3
Ajewole seyi <99263653+Sheyitofunmi@users.noreply.github.com>
2024-07-17 22:26:59 +0100

    new

--- src/components/layout/account-settings/index.tsx
+++ src/components/layout/account-settings/index.tsx
@@ -98,7 +98,7 @@ export const AccountSettings = ({ opened, setOpened, userId }: Props) => {
               shape="square"
               // src={avatarUrl}
               name={getNameInitials(name || "")}
-              imageSrc="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8ZmFjZXxlbnwwfHwwfHx8MA%3D%3D"
+              imageSrc="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZmFjZXxlbnwwfHwwfHx8MA%3D%3D"
               style={{
                 width: 96,
                 height: 96,
--- src/components/layout/current-user/index.tsx
+++ src/components/layout/current-user/index.tsx
@@ -70,7 +70,7 @@ export const CurrentUser = () => {
         /> */}
         <CustomAvatar
           name={user?.name}
-          imageSrc="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8ZmFjZXxlbnwwfHwwfHx8MA%3D%3D" // Provide the image URL here
+          imageSrc="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZmFjZXxlbnwwfHwwfHx8MA%3D%3D" // Provide the image URL here
           size="default"
           style={{ cursor: "pointer" }}
         />
